ac2c927b28ed1c75bcaaf00d52b49f1b198fc950
CurieLee <90137715+CurieLee@users.noreply.github.com>
2023-06-07 18:19:09 +0900

    Update UBERStudent20190985.java

--- HW3/UBERStudent20190985.java
+++ HW3/UBERStudent20190985.java
@@ -6,79 +6,84 @@ import org.apache.spark.api.java.function.*;
 
 import java.io.Serializable;
 import java.util.*;
-import java.time.*;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.TextStyle;
 
-public final class UBERStudent20190985 
+public final class UBERStudent20190985 implements Serializable 
 {
+	public static void main(String[] args) throws Exception 
+	{
+		
+		if (args.length < 2) 
+		{
+			System.err.println("Usage: UBER <in-file> <out-file>");
+			System.exit(1);
+		}
+		
+		SparkSession spark = SparkSession
+			.builder()
+			.appName("UBERStudent20190985")
+			.getOrCreate();
+		
+		JavaRDD<String> lines = spark.read().textFile(args[0]).javaRDD();
 
-    public static void main(String[] args) throws Exception 
-    {
+		PairFunction<String, String, String> pf = new PairFunction<String, String, String>() 
+		{
+			public Tuple2<String, String> call(String s) 
+			{
+				StringTokenizer itr = new StringTokenizer(s, ",");
 
-        if (args.length < 2) 
-        {
-            System.err.println("Usage: UBER <in-file> <out-file>");
-            System.exit(1);
-        }
+				String number = itr.nextToken().trim();
+				String date = itr.nextToken().trim();
+				
+				int year = Integer.parseInt(date.split("/")[2]);
+				int month = Integer.parseInt(date.split("/")[0]);
+				int day = Integer.parseInt(date.split("/")[1]);
+				
+				LocalDate d = LocalDate.of(year, month, day);
+				DayOfWeek dayOfWeek = d.getDayOfWeek();
+				
+				String dayEng=dayOfWeek.getDisplayName(TextStyle.SHORT,Locale.US).toUpperCase();;
+				if (date.equals("THU")) date = "THR";
+					    
+				String numDay = number + "," + dayEng;
+			    	
+			    	String vehicles = itr.nextToken().trim();
+			    	String trips = itr.nextToken().trim();
+			    	String vehtrip = trips + "," + vehicles;
+			    	
+				return new Tuple2(numDay, vehtrip);
+			}
+		};
+		
+		JavaPairRDD<String, String> pair = lines.mapToPair(pf);
 
-        SparkSession spark = SparkSession
-            .builder()
-            .appName("UBERStudent20190985")
-            .getOrCreate();
-
-        JavaRDD<String> lines = spark.read().textFile(args[0]).javaRDD();
-        
-        PairFunction<String, String, String> pf = new PairFunction<String, String, String>() 
-        {
-            public Tuple2<String, String> call(String s) 
-            {
-            	String line[] = s.split(",");
-            	String id = line[0];
-            	String date = line[1];
-            	String vehicles = line[2];
-            	String trips = line[3];
+        Function2<String, String, String> f2 = new Function2<String, String, String>() 
+	{
+            public String call(String x, String y) 
+	    {
+            	String[] vt1 = x.split(",");
+            	String[] vt2 = y.split(",");
             	
-            	String value = trips + "," + vehicles;
-            	String key = "";
-            	
-            	StringTokenizer itr2 = new StringTokenizer(date,"/");
-            	while (itr2.hasMoreTokens())
-            	{
-            		int month = Integer.parseInt(itr2.nextToken());
-			int day = Integer.parseInt(itr2.nextToken());
-			int year = Integer.parseInt(itr2.nextToken());
-			
-			LocalDate date_a = LocalDate.of(year,month,day);
-			DayOfWeek dayOfWeek = date_a.getDayOfWeek();
-			String result = dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.US);
-			String result_upper = result.toUpperCase();
-			key = id + "," + result_upper;
-            	}
-            	
-                return new Tuple2(key, value);
+            	int trips = Integer.parseInt(vt1[0]) + Integer.parseInt(vt2[0]);
+            	int vehicles = Integer.parseInt(vt1[1]) + Integer.parseInt(vt2[1]);
+            
+                return trips + "," + vehicles;
             }
         };
-      
-        JavaPairRDD<String, String> map1 = lines.mapToPair(pf);
-       
-        Function2<String, String, String> f2 = new Function2<String, String, String>() 
-        {
-        	public String call(String s1, String s2) 
-          {
-        		String[] line1 = s1.split(",");
-        		String[] line2 = s2.split(",");
+	
+        JavaPairRDD<String, String> sum = pair.reduceByKey(f2);
+        
+        JavaRDD<String> result = sum.map(x -> x._1 + " " + x._2);
 
-        		int trip = Integer.parseInt(line1[0]) + Integer.parseInt(line2[0]);
-        		int vehicle = Integer.parseInt(line1[1]) + Integer.parseInt(line2[1]);
-        		
-        		String value = trip + "," + vehicle;
-        		
-        		return value; 		
-        	}
-        };
-      
-	JavaPairRDD<String, String> reduce1 = map1.reduceByKey(f2);
-       
-        reduce1.saveAsTextFile(args[1]);
+        result.saveAsTextFile(args[1]);
         spark.stop();
     }
 }
